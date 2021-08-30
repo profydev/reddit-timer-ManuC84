632b4db0ceb6c43c926139628488718718f76875
Manu <mcufre84@gmail.com>
2021-08-30 19:37:16 +0200

    changed double quotes to single quotes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "normalize.css";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import 'normalize.css';
 
 function App() {
   return (
